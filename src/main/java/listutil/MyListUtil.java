b219ca0605239365b63950f69d1ede98216e6620
Piero Bassa <pierobassa222@gmail.com>
2021-10-28 09:10:07 +0200

    inizializzazione MyListUtil

---
+++ src/main/java/listutil/MyListUtil.java
@@ -0,0 +1,12 @@
+package listutil;
+
+import java.util.List;
+import java.util.Vector;
+
+public class MyListUtil {
+	private Vector<Integer> list;
+	
+	public List<Integer> sort(List<Integer> list, int order){   //order = 0 Crescente, 1 decrescente
+		
+    }
+}
