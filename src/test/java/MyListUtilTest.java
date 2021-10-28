dab293eca2ba1091f5bdf4069945b8c844476df7
Piero Bassa <pierobassa222@gmail.com>
2021-10-28 09:28:55 +0200

    Completed descendent sort test

--- src/test/java/MyListUtilTest.java
+++ src/test/java/MyListUtilTest.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.Vector;
 
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -37,5 +38,27 @@ public class MyListUtilTest {
 		}
 		
 		Assert.assertArrayEquals(expected, orderRs);
+	}
+	
+	@Test
+	public void testSortDecrescente() {
+		Integer[] numbers = {6, 2, 1, 9, 3, 4};
+		Integer[] expected = {9, 6, 4, 3, 2, 1};
+		
+		Vector<Integer> numbersV = new Vector<Integer>(Arrays.asList(numbers));
+		Vector<Integer> orderResult = (Vector<Integer>) myListUtil.sort(numbersV, 1);
+		
+		Integer[] orderRs = new Integer[orderResult.size()];
+		
+		for(int i=0; i<orderRs.length; i++) {
+			orderRs[i] = orderResult.elementAt(i);
+		}
+		
+		Assert.assertArrayEquals(expected, orderRs);
+	}
+	
+	@AfterClass
+	public static void afterClass() {
+		System.out.println("Testing completed");
 	}
 }
